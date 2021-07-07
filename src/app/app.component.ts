d921ff23de034f05ec11e117b2a1f9a6c39ac060
Owais Parkar <Owais.Parkar@pinnacle-corp.co.nz>
2021-07-07 13:42:16 +1200

    getting coordinates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.getLocation();
+  }
   title = 'angular-maps';
+  url: string = '';
+
+  getLocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const longitude = position.coords.longitude;
+        const latitude = position.coords.latitude;
+        console.log('here')
+        this.callApi(longitude, latitude);
+      });
+    } else {
+      console.log("No support for geolocation")
+    }
+  }
+
+  callApi(Longitude: number, Latitude: number) {
+    this.url = `https://gps-coordinates.org/my-location.php?lat=${Latitude}&lng=${Longitude}`;
+  }
 }
